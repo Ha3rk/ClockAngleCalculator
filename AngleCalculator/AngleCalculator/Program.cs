210d460dba2b9aa84ee74fe7b25c1966318c4345
Akeem Ajibare <88968613+iambrandhakim@users.noreply.github.com>
2023-02-22 06:16:11 +0200

    Add files via upload

---
+++ AngleCalculator/AngleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Calculator
+    {
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new Input();
+            var clock = new Clock();
+            var output = new Output();
+
+            var hours = input.GetHours();
+            var minutes = input.GetMinutes();
+
+            var angle = clock.CalculateAngle(hours, minutes);
+            output.DisplayAngle(angle);
+        }
+    }
+}
